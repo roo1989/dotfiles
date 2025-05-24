2454d8b247ee50a5d026882f1a204d6b1dba6ad5
Ragnar Olafsson <ragnar.orn@gmail.com>
2025-05-24 09:55:24 +0000

    add a keymap to reload lua modules

--- nvim/lua/roo/keymaps.lua
+++ nvim/lua/roo/keymaps.lua
@@ -12,4 +12,13 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<leader>rr", function()
+	for name, _ in pairs(package.loaded) do
+		if name:match("^roo") then -- match your namespace
+			package.loaded[name] = nil
+		end
+	end
+	dofile(vim.env.MYVIMRC)
+	print("Reloaded config and modules.")
+end, { desc = "Reload config + modules" })
