4792591820381d2dae06b84a47482437dd1d2be2
Ragnar Olafsson <ragnar.orn@gmail.com>
2026-01-12 13:09:49 +0000

    remove nvim

--- nvim/lua/roo/lazy/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"dgox16/oldworld.nvim",
-	lazy = false,
-	priority = 1000,
-
-	config = function()
-		vim.cmd.colorscheme("oldworld")
-	end,
-}
--- nvim/lua/roo/lazy/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"nvim-lua/plenary.nvim",
-		name = "plenary",
-	},
-
-	"github/copilot.vim",
-	"gpanders/editorconfig.nvim",
-}
