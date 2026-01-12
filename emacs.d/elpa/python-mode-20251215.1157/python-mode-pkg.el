2880bbec325439ce03170958a970964fb98e235d
Ragnar Olafsson <ragnar.orn@gmail.com>
2026-01-12 13:11:49 +0000

    Stop tracking ELPA packages

--- emacs.d/elpa/python-mode-20251215.1157/python-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "python-mode" "20251215.1157"
-  "Python major mode."
-  ()
-  :url "https://gitlab.com/groups/python-mode-devs"
-  :commit "5719f9a18c4813303df6c8102ac25fce8e4536d0"
-  :revdesc "5719f9a18c48"
-  :keywords '("python" "languages" "oop")
-  :maintainers '((nil . "python-mode@python.org")))
