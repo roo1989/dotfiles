829f155b42be608a67aad319df67e0153a6fbf99
Ragnar Olafsson <ragnar.orn@gmail.com>
2026-01-14 14:26:30 +0000

    forgot to tangle

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -598,3 +598,16 @@
     (setq eshell-visual-commands '("htop" "zsh" "vim")))
 
   (eshell-git-prompt-use-theme 'powerline))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(warning-suppress-types '((auto-save))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
